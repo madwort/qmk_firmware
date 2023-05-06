3cf2367116fe1e169f79569e49427f319485e512
Tom Ward <tomward@fmail.co.uk>
2023-05-06 11:09:00 +0100

    put grave onto LH little finger for switching between windows

--- keyboards/splitkb/kyria/keymaps/madwort/keymap.c
+++ keyboards/splitkb/kyria/keymaps/madwort/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |  Tab   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  Enter |
+ * |  ` ~   |   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  Enter |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |   Esc  |LShift|   X  |   C  |   V  |   B  |  ` ~ |      |  |VolMut|  \ | |   N  |   M  | ,  < | . >  | /  ? | RShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
@@ -74,10 +74,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
-     CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN, KC_ENTER,
-     KC_ESCAPE , KC_LSFT , KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_GRAVE,  _______,   KC_MUTE  , KC_BACKSLASH, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                KC_Z , ADJUST, KC_LGUI, KC_BSPC , KC_LEFT_CTRL,     KC_LALT    , KC_SPC , NAVSYM, KC_LEFT_BRACKET, KC_QUOTE
+     KC_TAB,    KC_Q,    KC_W,  KC_E, KC_R,   KC_T,                                     KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
+     KC_GRAVE,  KC_A,    KC_S,  KC_D, KC_F,   KC_G,                                     KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN, KC_ENTER,
+     KC_ESCAPE, KC_LSFT, KC_X,  KC_C, KC_V,   KC_B,    KC_GRAVE, _______     , KC_MUTE, KC_BACKSLASH, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
+                                KC_Z, ADJUST, KC_LGUI, KC_BSPC , KC_LEFT_CTRL, KC_LALT, KC_SPC , NAVSYM, KC_LEFT_BRACKET, KC_QUOTE
     ),
 
 /*
